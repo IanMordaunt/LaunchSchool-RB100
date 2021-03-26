6f85c28362275d7c13fa09ce1dff9e47c547988f
Ian Mordaunt <Ianmordaunt@gmail.com>
2021-03-25 22:45:14 -0600

    Adding File:Exercise_3.rb

---
+++ Flow_Control/Exercise_3.rb
@@ -0,0 +1,12 @@
+puts "Pick a number!"
+n = gets.chomp.to_i
+
+if n <= 50
+  puts "#{n} is between 0 and 50"
+elsif n <= 100
+  puts "#{n} is between 50 and 100"
+else 
+  puts "#{n} is above 100"
+end
+
+
