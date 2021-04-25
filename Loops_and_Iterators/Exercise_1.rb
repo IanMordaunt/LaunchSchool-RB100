02d9cd659fed03d97e1caa09f9a2ee90845eb8d6
Ian Mordaunt <Ianmordaunt@gmail.com>
2021-04-24 23:46:41 -0600

    adding file: Exercise_1.rb

--- Loops_and_Iterators/Exercise_1.rb
+++ Loops_and_Iterators/Exercise_1.rb
@@ -1,3 +1,10 @@
-kjsdfh;skjfg bhsldk gjfb
-.,ekjsdfgbhl.kdfsgjb
-.dskgfjhdbflgkjh
+
+
+x = " "
+while x != "STOP" do
+  puts "Hi, How are you feeling?"
+  answer = gets.chomp
+  puts "Want me to ask you again?"
+  x = gets.chomp
+end
+
