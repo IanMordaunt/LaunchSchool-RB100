a974f1b3feec3f44a1fa25c0f5f91a7896e9c47c
Ian Mordaunt <Ianmordaunt@gmail.com>
2021-04-26 22:55:13 -0600

    Adding Directory: Arrays

---
+++ Arrays/Exercise_7.rb
@@ -0,0 +1,8 @@
+
+
+animals = ['parrot', 'monkey', 'jaguar', 'panda', 'armadillo']
+
+animals.each_with_index do |types, index|
+  puts "#{index + 1}. #{types}"
+end
+
---
+++ Arrays/Exercise_8.rb
@@ -0,0 +1,11 @@
+
+
+array  = [1, 2, 3, 4, 5]
+new_array = []
+
+array.each do |x|
+  new_array << x + 2
+end
+
+p array
+p new_array
