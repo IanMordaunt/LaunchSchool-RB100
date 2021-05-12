f204c80dd66cd83db89c6be9862652f56573009d
Ian Mordaunt <Ianmordaunt@gmail.com>
2021-05-11 20:44:04 -0600

    Correction to: Hashes/Excercise_3.rb

--- Hashes/Exercise_3.rb
+++ Hashes/Exercise_3.rb
@@ -12,4 +12,4 @@ car.each_key { |key| puts key }
 
 car.each_value { |value| puts value }
 
-car.each { |key, value| puts "The catagory is #{key} and detail is #{value}" }
+car.each { |key, value| puts "The key is #{key} and the value is #{value}" }
